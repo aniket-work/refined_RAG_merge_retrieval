0626539508648d7101409252d37def56053624b9
worka <study.aniket.hingane@gmail.com>
2024-01-20 13:04:32 -0500

    Added dedicated client

--- byod_bring_ur_doc.py
+++ byod_bring_ur_doc.py
@@ -1,28 +1,25 @@
-import json
 from embedding_model import EmbeddingModel
+from document_loader import DocumentLoader
+from text_splitter import TextSplitter
 from vector_space import VectorSpace
-from merger_retriever import MergerRetrieverHandler
+from merger_retriever import MergerRetriever
+import os
 
 # Part 1: Get the Embedding Model
 model_name = "BAAI/bge-large-en"
 model = EmbeddingModel(model_name)
 
 # Part 4: Load Vector vector_space
-vector_spaces_config = "vector_spaces.json"
-
-with open(vector_spaces_config, 'r') as file:
-    vector_spaces_data = json.load(file)
-
+pdf_files = ["data/000885.pdf", "data/000945.pdf"]
 vector_spaces = []
 
-for space_data in vector_spaces_data["vector_spaces"]:
-    vector_space_name = space_data["name"]
-    vector_space = VectorSpace([], model, f"vector_space/{vector_space_name}")
+for file_path in pdf_files:
+    vector_space = VectorSpace([], model, f"vector_space/{os.path.basename(file_path)}_chroma_cosine")
     vector_spaces.append(vector_space.load_vector_space())
 
 # Part 5: Init Merge Retriever
 retrievers = [vs.as_retriever(search_type="similarity", search_kwargs={"k": 3}) for vs in vector_spaces]
-lotr = MergerRetrieverHandler(retrievers)
+lotr = MergerRetriever(retrievers)
 
 query = "marine protected areas are being designated according which approaches?"
 for chunks in lotr.get_relevant_documents(query):
